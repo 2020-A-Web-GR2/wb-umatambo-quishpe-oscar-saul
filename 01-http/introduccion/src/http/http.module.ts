3e135ac6f6bb39a1d3d02f02a290d47ea759876f
Oscar Umatambo <oscar.umatambo@epn.edu.ec>
2020-07-02 15:58:11 -0500

    Clase 9

---
+++ 01-http/introduccion/src/http/http.module.ts
@@ -0,0 +1,11 @@
+import {Module} from "@nestjs/common";
+
+// @Nombre() ------> esto es un decorador
+@Module({
+    imports: [ ],
+    controllers: [ ],
+    providers: [ ],
+})
+export class HttpModule{
+
+}
